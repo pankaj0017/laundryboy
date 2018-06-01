fc00cf7bb4aec0dfcb78bf492aa2757952ee045b
Pankaj Jaiswal <pkj0017@gmail.com>
2018-06-01 14:24:06 +0530

    google auth done

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 var express        = require("express"),
     app            = express(),
+    port           = process.env.PORT || 8080;
     bodyParser     = require("body-parser"),
     mongoose       = require("mongoose"),
     passport       = require("passport"),
-    LocalStrategy  = require("passport-local"),
-    User           = require("./models/user"),
-    Customer       = require("./models/customer"),
-    Order          = require("./models/order"),
-    Vendor         = require("./models/vendor"),
-    DeliveryBoy    = require("./models/deliveryboy"),
-    LaundryBoy     = require("./models/laundryboy"),
+    // LocalStrategy  = require("passport-local"),
+    // User           = require("./models/user"),
+    session        = require('express-session'),
+    // auth           = require("./config/auth"),
+    // Customer       = require("./models/customer"),
+    // Order          = require("./models/order"),
+    // Vendor         = require("./models/vendor"),
+    // DeliveryBoy    = require("./models/deliveryboy"),
+    // LaundryBoy     = require("./models/laundryboy"),
     methodOverride = require("method-override"),
-    morgan         = require("morgan");
+    morgan         = require("morgan"),
+    cookieParser   = require('cookie-parser'),
+    flash          = require('connect-flash');
 
 mongoose.connect("mongodb://localhost/laundrybuoy");
 app.use(morgan('dev'));
+app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
@@ -28,66 +34,64 @@ app.use(require("express-session")({
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   next();
-});
+require('./config/passport')(passport); // pass passport for configuration
 
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
-app.get("/", function(req, res){
-    res.render("landing");
-});
-
-// show register form
-app.get("/register", function(req, res){
-   res.render("register"); 
-});
-
-//handle sign up logic
-app.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function(){
-           res.redirect("/"); 
-        });
-    });
-});
-
-// show login form
-app.get("/login", function(req, res){
-   res.render("login"); 
-});
-// handling login logic
-app.post("/login", passport.authenticate("local", 
-    {
-        successRedirect: "/",
-        failureRedirect: "/login"
-    }), function(req, res){
-});
-
-// logic route
-app.get("/logout", function(req, res){
-   req.logout();
-   res.redirect("/");
-});
-
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
+// app.use(function(req, res, next){
+//    res.locals.currentUser = req.user;
+//    next();
+// });
 
 
-app.listen(3005, function(){
+// app.get("/", function(req, res){
+//     res.render("landing");
+// });
+
+// // show register form
+// app.get("/register", function(req, res){
+//    res.render("register"); 
+// });
+
+// //handle sign up logic
+// app.post("/register", function(req, res){
+//     var newUser = new User({username: req.body.username});
+//     User.register(newUser, req.body.password, function(err, user){
+//         if(err){
+//             console.log(err);
+//             return res.render("register");
+//         }
+//         passport.authenticate("local")(req, res, function(){
+//            res.redirect("/"); 
+//         });
+//     });
+// });
+
+// // show login form
+// app.get("/login", function(req, res){
+//    res.render("login"); 
+// });
+// // handling login logic
+// app.post("/login", passport.authenticate("local", 
+//     {
+//         successRedirect: "/",
+//         failureRedirect: "/login"
+//     }), function(req, res){
+// });
+
+
+
+
+app.listen(port, function(){
    console.log("The LaundryBuoy Server Has Started!");
 });
+
+
+// google client id: 197999679029-0o911bog5lapuna69quvvh4rkc8crght.apps.googleusercontent.com
+//google client secret: 8qbKB59CdpkKQmoUYBLHY1vx
---
+++ config/auth.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    'googleAuth' : {
+        'clientID'      : '197999679029-0o911bog5lapuna69quvvh4rkc8crght.apps.googleusercontent.com', // your App ID
+        'clientSecret'  : '8qbKB59CdpkKQmoUYBLHY1vx', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'facebookAuth' : {
+        'clientID'      : '644077089273017',
+        'clientSecret'  : '8a1398ae53e6c6a09524198937a596e4',
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    }
+
+};
