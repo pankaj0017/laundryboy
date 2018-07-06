9363bab68ebef760d3bc7213c40161974d26ea2b
Pankaj Jaiswal <pkj0017@gmail.com>
2018-07-06 14:06:00 +0530

    order backend

--- app/models/order.js
+++ app/models/order.js
@@ -5,6 +5,7 @@ var OrderSchema = mongoose.Schema({
 	customer:         {type: mongoose.Schema.Types.ObjectId, ref: "Customer"},	 		//when order is placed
     vendor:           {type: mongoose.Schema.Types.ObjectId, ref: "Vendor"},			//when order is placed
     deliveryBoy:      {type: mongoose.Schema.Types.ObjectId, ref: "DeliveryBoy"},		//when delivered
+    isIron:           {type: Boolean , default: false},
     cost:             {type: Number , default: 0},        
     description:      {type: String , default: ''},
     status:           {type: String , default: 'booked'},           //booked, picked, washed, out, delivered,paid, terminated
