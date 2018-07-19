816e09696bd827c2db3c4c17664aa3215242f0e8
Pankaj Jaiswal <pkj0017@gmail.com>
2018-07-19 20:28:41 +0530

    front end

--- config/auth.js
+++ config/auth.js
@@ -9,7 +9,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1733790833364047',
         'clientSecret'  : '4bf7ef221bae588aa997b08a28fc5b54',
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'   : 'https://localhost:8080/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     }
